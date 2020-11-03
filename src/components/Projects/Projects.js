1940cde03bed3ee8196fd43cfa8d5c31be529e6c
Alexey <ololmkbc@gmail.com>
2020-11-03 12:20:48 +0300

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { Carousel, Container } from 'react-bootstrap';
-import pic1 from '../../pics/pic1.jpg'
-import pic2 from '../../pics/pic2.jpg'
-import pic3 from '../../pics/pic3.jpg'
+import { Carousel } from 'react-bootstrap';
 import './style.scss';
 
 
 import weather from '../../pics/weather/weather.png'
-import weather1 from '../../pics/weather/weather1.png'
-import weather2 from '../../pics/weather/weather2.png'
-import weather3 from '../../pics/weather/weather3.png'
 
 const Projects = () => {
     return (
@@ -30,10 +24,7 @@ const Projects = () => {
                             alt="First slide"
                             src={weather}
                         />
-                    </div> <Carousel.Caption>
-                        <h5>Weather forecast</h5>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
+                    </div>
                 </Carousel.Item>
 
 
@@ -42,14 +33,11 @@ const Projects = () => {
                         <img
                             className="w-100"
                             alt="First slide"
-                            src={pic1}
+                            src={weather}
                         />
                     </div>
-                    <Carousel.Caption>
-                        <h5>Mini Gallery</h5>
-                        <p>MobX</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
+
 
             </Carousel>
         </div>
