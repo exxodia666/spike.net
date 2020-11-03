b0e851af1006edfe742cddce00793497d26425ea
Alexey <42065561+exxodia666@users.noreply.github.com>
2020-11-03 11:34:28 +0200

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import { Carousel, Container } from 'react-bootstrap';
-import pic1 from '../../pics/pic1.jpg'
-import pic2 from '../../pics/pic2.jpg'
-import pic3 from '../../pics/pic3.jpg'
 import './style.scss';
-
-
 import weather from '../../pics/weather/weather.png'
-import weather1 from '../../pics/weather/weather1.png'
-import weather2 from '../../pics/weather/weather2.png'
-import weather3 from '../../pics/weather/weather3.png'
+//import gallery from '../../pics/gallery/gallery.png'
 
 const Projects = () => {
     return (
         <div id='projects'>
             <h4 className="display-4">Projects</h4>
-            <Carousel prevIcon={<span aria-hidden="false"
-                style={{ backgroundColor: 'black' }}
-                className="carousel-control-prev-icon"
-            />}
-                nextIcon={<span aria-hidden="false"
-                    style={{ backgroundColor: 'black', color: 'red' }}
-                    className="carousel-control-next-icon"
-                />}>
+            <Carousel>
                 <Carousel.Item>
                     <div style={{ display: "flex", justifyContent: "center" }}>
                         <img
@@ -31,8 +17,8 @@ const Projects = () => {
                             src={weather}
                         />
                     </div> <Carousel.Caption>
-                        <h5>Weather forecast</h5>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                      {/*  <h5>Weather forecast</h5>
+                        {/*<p>Description</p>*/}
                     </Carousel.Caption>
                 </Carousel.Item>
 
@@ -42,12 +28,12 @@ const Projects = () => {
                         <img
                             className="w-100"
                             alt="First slide"
-                            src={pic1}
+                            src={weather}
                         />
                     </div>
                     <Carousel.Caption>
-                        <h5>Mini Gallery</h5>
-                        <p>MobX</p>
+                       {/* <h5>Mini Gallery</h5>
+                        {/*<p>Description</p>*/}
                     </Carousel.Caption>
                 </Carousel.Item>
 
